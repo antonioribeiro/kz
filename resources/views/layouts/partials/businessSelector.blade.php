5e8e4ccc41d6906ebddcf88ecad634b39265672a
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-11-05 16:22:13 +0000

    Fix user relation

--- resources/views/layouts/partials/businessSelector.blade.php
+++ resources/views/layouts/partials/businessSelector.blade.php
@@ -2,7 +2,12 @@
     <li class="dropdown navbar-user">
         <div class="pull-right">
             <div class="btn-group">
-                <button type="button" class="btn btn-success btn-xs">empresa - <strong>{{ $current_business->name }}</strong></button>
+                <button type="button" class="btn btn-inverse btn-xs">empresa</button>
+
+                @if ($current_business->name)
+                    <button type="button" class="btn btn-success btn-xs"><strong>{{ strtoupper($current_business->name) }}</strong></button>
+                @endif
+
                 @if ($all_available_businesses)
                     <button type="button" class="btn btn-success btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                         <span class="caret"></span>
@@ -13,11 +18,22 @@
                             <li><a href="{{ route('businesses.enterprises.select', ['businessId' => $business->id]) }}">{{ $business->name }}</a></li>
                         @endforeach
                     </ul>
+                @else
+                    @if (! $current_business->name)
+                        <button type="button" class="btn btn-primary btn-xs">não disponível</button>
+                    @endif
                 @endif
             </div>
 
+            &nbsp;&nbsp;&nbsp;
+
             <div class="btn-group">
-                <button type="button" class="btn btn-primary btn-xs">cliente - <strong>{{ $current_business_client->name }}</strong></button>
+                <button type="button" class="btn btn-inverse btn-xs">cliente</button>
+
+                @if ($current_business_client->name)
+                    <button type="button" class="btn btn-primary btn-xs"><strong>{{ strtoupper($current_business_client->name) }}</strong></button>
+                @endif
+
                 @if ($all_available_business_clients)
                     <button type="button" class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
                         <span class="caret"></span>
@@ -28,6 +44,10 @@
                             <li><a href="{{ route('businesses.enterprises.select.client', ['clientId' => $client->id]) }}">{{ $client->name }}</a></li>
                         @endforeach
                     </ul>
+                @else
+                    @if (! $current_business_client->name)
+                        <button type="button" class="btn btn-primary btn-xs">não disponível</button>
+                    @endif
                 @endif
             </div>
         </div>
